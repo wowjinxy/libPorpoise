2039155c4cc230881dd46056be1c033e6ca106c7
wowjinxy <1340990+wowjinxy@users.noreply.github.com>
2025-11-11 16:48:53 -0600

    Add stdio.h include to OSTime.c for sprintf
    OSTimeToString() uses sprintf but stdio.h was not included. Added missing include.

--- src/os/OSTime.c
+++ src/os/OSTime.c
@@ -152,6 +152,7 @@
  *---------------------------------------------------------------------------*/
 
 #include <dolphin/os.h>
+#include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
 #include <string.h>
