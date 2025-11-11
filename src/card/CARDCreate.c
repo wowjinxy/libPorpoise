3469020d3c7835a83673423d9ba7f3f4c970ec66
wowjinxy <1340990+wowjinxy@users.noreply.github.com>
2025-11-10 18:14:15 -0600

    CARDCreate now tracks created files for Read/Write operations

--- src/card/CARDCreate.c
+++ src/card/CARDCreate.c
@@ -8,6 +8,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <sys/stat.h>
+#include <stdlib.h>
 
 #ifdef _WIN32
 #define stat _stat
@@ -61,13 +62,30 @@ s32 CARDCreateAsync(s32 chan, const char* fileName, u32 size,
     
     fclose(file);
     
+    // Find free file slot and store filename
+    s32 fileNo = -1;
+    for (int i = 0; i < 127; i++) {
+        if (__CARDCards[chan].openFiles[i][0] == '\0') {
+            fileNo = i;
+            break;
+        }
+    }
+    
+    if (fileNo < 0) {
+        return CARD_RESULT_LIMIT;
+    }
+    
+    // Store filename for read/write operations
+    strncpy(__CARDCards[chan].openFiles[fileNo], fileName, CARD_FILENAME_MAX - 1);
+    __CARDCards[chan].openFiles[fileNo][CARD_FILENAME_MAX - 1] = '\0';
+    
     fileInfo->chan = chan;
-    fileInfo->fileNo = 0;
+    fileInfo->fileNo = fileNo;
     fileInfo->offset = 0;
     fileInfo->length = size;
     fileInfo->iBlock = 0;
     
-    OSReport("CARD: Created '%s' (%u bytes)\n", fileName, size);
+    OSReport("CARD: Created '%s' (%u bytes) [fileNo=%d]\n", fileName, size, fileNo);
     
     if (callback) {
         callback(chan, CARD_RESULT_READY);
