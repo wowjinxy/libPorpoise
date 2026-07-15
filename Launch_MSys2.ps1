9881fff1ddb5c2ed4804dcd6adcabdf93e894420
cybervisi0n <112425559+cybervisi0n@users.noreply.github.com>
2026-07-14 19:31:24 -0500

    Remove cmake, attempt to fix windows build, improve linux build workflow

--- Launch_MSys2.ps1
+++ Launch_MSys2.ps1
@@ -5,7 +5,7 @@ param (
 set MSYSTEM=MINGW
 $msys2pwd=.\msys2\usr\bin\bash.exe -c "pwd"
 if ($command) {
-	.\msys2\usr\bin\bash.exe --rcfile "$msys2pwd/tools/scripts/env_vars_win64.sh" -ci "$command"
+	.\msys2\usr\bin\bash.exe --rcfile "$msys2pwd/standalone/scripts/env_vars_win64.sh" -ci "$command"
 } else {
-	.\msys2\usr\bin\bash.exe --rcfile "$msys2pwd/tools/scripts/env_vars_win64.sh"
+	.\msys2\usr\bin\bash.exe --rcfile "$msys2pwd/standalone/scripts/env_vars_win64.sh"
 }
