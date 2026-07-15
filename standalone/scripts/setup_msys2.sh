45e19acc1d0d0c7a56ea9c5f33c0c86c313bf0a5
cybervisi0n <112425559+cybervisi0n@users.noreply.github.com>
2026-07-15 16:58:43 -0500

    CI: fix SDL2 not found on windows

--- standalone/scripts/setup_msys2.sh
+++ standalone/scripts/setup_msys2.sh
@@ -2,5 +2,5 @@
 
 echo Setting up MSys2 environment!
 
-pacman -S --noconfirm make p7zip git python3 meson flex bison ninja mingw-w64-ucrt-x86_64-gcc mingw-w64-ucrt-x86_64-SDL2 zip
+pacman -S --noconfirm make p7zip git python3 flex bison mingw-w64-ucrt-x86_64-meson mingw-w64-ucrt-x86_64-ninja mingw-w64-ucrt-x86_64-gcc mingw-w64-ucrt-x86_64-SDL2 zip
 git config --global core.autocrlf true
