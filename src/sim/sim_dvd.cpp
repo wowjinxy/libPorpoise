83ba4a6d35875640692a86ec4ed810e5c1e511da
cybervisi0n <112425559+cybervisi0n@users.noreply.github.com>
2026-08-11 17:22:46 -0500

    DVD: Fix windows paths

--- src/sim/sim_dvd.cpp
+++ src/sim/sim_dvd.cpp
@@ -47,6 +47,7 @@ static constexpr auto RootPathName = "DVDRoot";
 
 void Init() {
     auto exeDir = GetExeDir();
+    std::replace(exeDir.begin(), exeDir.end(), '\\', '/');
     s_dvdRootPath = exeDir + "/" + RootPathName;
 
     // Create DVDRoot if it does not exist
@@ -58,6 +59,7 @@ void Init() {
     for (const auto& dirEntry : recursive_directory_iterator(s_dvdRootPath)) {
         auto entryNum = s_dvdEntrynumIndex.size();
         auto pathStr = dirEntry.path().string();
+        std::replace(pathStr.begin(), pathStr.end(), '\\', '/');
         
         s_dvdEntrynumMap.emplace(pathStr, static_cast<s32>(entryNum));
         s_dvdEntrynumIndex.push_back(pathStr);
@@ -72,8 +74,11 @@ const char * GetRootPath() {
 }
 
 s32 ConvertPathToEntrynum(const char * path) {
-    if(s_dvdEntrynumMap.count(path)) {
-        return s_dvdEntrynumMap[path];
+    std::string newPath(path);
+    std::replace(newPath.begin(), newPath.end(), '\\', '/');
+    
+    if(s_dvdEntrynumMap.count(newPath)) {
+        return s_dvdEntrynumMap[newPath];
     } else {
         return -1;
     }
